324512ee2ddca308c0a5c78af7430b5bfaecf0f9
ARVIND <arvindkmr2393@gmail.com>
2016-11-30 00:03:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports={
 
-    consumer_key:         'eid5C06paF8fXwc9puzqzltQb',
-    consumer_secret:      '3RDD9VtcXbRLfKjLc4YIeIurfvXVlexHXemkVAwn2iTMhJlXyt',
-    access_token:         '2683657872-us2AVm7aJk7YVMH1qkJIvwaA6APcJKuVVAS0KFy',
-    access_token_secret:  'pmc1ZpUtfLODtD29aZv7hETjQTH8onzKLE4oo0wcBZrRM'
+    consumer_key:         'use your own',
+    consumer_secret:      'use your own',
+    access_token:         'use your own',
+    access_token_secret:  'use your own'
 
 }
